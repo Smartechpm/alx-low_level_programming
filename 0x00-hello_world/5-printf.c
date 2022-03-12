07cdd9d9bcdba120eccd67579ed5d9d67ef940c0
Smartechpm <filimonimusa3@gmail.com>
2022-03-11 16:22:16 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
 /**
  *main -entry point
- *
- *Hello, printf
- *Return: 0 after execution
+ * 
+ *Hello, printf task
+ *Return: 1 after execution
  */
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
 return (0);
 }
-
