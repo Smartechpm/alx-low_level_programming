81cf1a6cc9699b122bf5047ac0690984cb109a0e
Smartechpm <filimonimusa3@gmail.com>
2022-03-14 16:42:09 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main -entry point
+ *
+ * printing single digit numbers of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int digits;
+
+for (digits = 0; digits < 10; digits++)
+{
+printf("%d \n", digits);
+}
+return (0);
+}
+
