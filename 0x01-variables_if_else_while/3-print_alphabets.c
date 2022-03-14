d96281593b5ffebb412d0f2656286ae49ffda16b
Smartechpm <filimonimusa3@gmail.com>
2022-03-14 16:11:48 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * printing both lowercase and uppercase alphabets
+ *
+ * Return; Always 0 (Success/correct)
+ */
+int main(void)
+{
+int alphaBETS;
+
+for(alphaBETS = 'a'; alphaBETS <= 'z'; alphaBETS++)
+{
+putchar(alphaBETS);
+}
+for(alphaBETS = 'A'; alphaBETS <= 'Z'; alphaBETS++)
+{
+putchar(alphaBETS);
+}
+putchar('\n');
+
+return (0);
+}
+
