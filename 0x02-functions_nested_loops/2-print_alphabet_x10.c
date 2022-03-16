39e4a168deac80749bf968d660815b13b52ee01a
Smartechpm <filimonimusa3@gmail.com>
2022-03-16 16:27:43 -0700

    print_alphabet_10_times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,12 @@ void print_alphabet_x10(void)
 {
 char alphabet;
 int i = 0;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-{
 while (i < 10)
 {
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
 _putchar(alphabet);
+}
 _putchar('\n');
 i++;
 }
