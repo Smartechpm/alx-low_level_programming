5b6f8945fc11b799b05bc6a98d34dafdcfe3506b
Smartechpm <filimonimusa3@gmail.com>
2022-03-16 15:49:12 -0700

    0-alphabet.C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -entry point.
+ * Description: print lowercase alphabets.
+ * Return: void.
+ */
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0x02-functions_nested_loops
 0-putchar.c: program that prints _putchar, followed by a new line.
+1-alphabet.c: function that prints the alphabet, in lowercase, followed by a new line.
