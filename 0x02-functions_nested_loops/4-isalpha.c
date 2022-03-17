1be9f8f05a9fd3e2b3fad8228c9d69ab7951c238
Smartechpm <filimonimusa3@gmail.com>
2022-03-16 17:05:52 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha -check for alphabetic letter for both upper or lower case
+ *
+ * @c: input.
+ * Return: 1 if c is alphabetic
+ * 0 if c is anything else.
+ */
+int _isalpha(int c)
+{
+if ((c >+ 'A' && C <= 'Z') || (C >= 'a' && c <= 'z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
