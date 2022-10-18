b116a56d16dfc00d65861278253d2d2ec1842015
Eng.Philemon <filimonimusa3@gmail.com>
2022-10-18 00:05:55 +0000

    Static libraries project

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+gcc -Wall -Werror -Wextra -pedantic -c std=gnu89 *.c
+ar -rcs liball.a *.o
+nm liball.a
 
